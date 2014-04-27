0b51b6c6449276c61099cc667e717cf51605bbb0
ashleydavis <ashley@codecapers.com.au>
2014-04-28 08:46:02 +1000

    Added a level of indirection between the external interface and the internal route init code.

---
+++ route_init.js
@@ -0,0 +1,8 @@
+//
+// Functions for initializing the routes.
+//
+
+module.exports = function (config, app) {
+
+	console.log('test');
+};
--- routey.js
+++ routey.js
@@ -1,14 +1,16 @@
+var route_init = require('./route_init');
 
 //
 // Params
 // 	config			-> Routey configuration.
 //
-// 		routesDir 	-> Directory that contains routes.
+// 		routesDir 	-> Directory that contains route definitions.
+//		urlRoot		-> Route path for route URLs.
+//		verbose		-> Enable verbose logging so you can see what is happening.
 //	
 //	app				-> Express application object for route setup.
 //
 module.exports = function (config, app) {
 
-	console.log('test');
-
+	route_init(config, app);
 };
